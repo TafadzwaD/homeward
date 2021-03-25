f8bfeb46bcbcc5e07763a4ed7616512c57eb2c35
TafadzwaD <tafadzwademba@gmail.com>
2021-03-25 14:57:44 +0200

    Login Screen Widget Test.. Test To see if there are two textfields(one for email and the other for password.. Used and elevated login button so we also test if the LoadingElevated button is being displayed on the login screen.. If any of the case fails the test will fail

---
+++ test/views/login_screen_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:homeward/providers/authentication_provider.dart';
+import 'package:homeward/providers/blogs_provider.dart';
+import 'package:homeward/utilities/all_providers.dart';
+import 'package:homeward/views/all_blogs/blogs_screen.dart';
+import 'package:homeward/views/login/login_screen.dart';
+import 'package:loading_elevated_button/loading_elevated_button.dart';
+import 'package:provider/provider.dart';
+
+void main(){
+
+  loginScreen() =>  MultiProvider(
+    providers: providers,
+    child: MaterialApp(
+
+      home: Consumer<AuthProvider>(
+        builder: (context, state, child) {
+          return state.isLoggedIn ? BlogsHomeScreen() : LoginScreen();
+        },
+      ),
+
+    ),
+  );
+
+
+  group('Login Screen Widgets Test : ', (){
+      testWidgets('Testing if 2 TextFields Show Up on the Login Screen(Email Field and Password Field)', (tester) {
+         return tester.pumpWidget(loginScreen()).then((value){
+            expect(find.byType(TextField),findsNWidgets(2));
+          });
+      });
+      testWidgets('Testing if 1 Loading Elevated button for Login Show Up on the Login Screen', (tester) {
+         return tester.pumpWidget(loginScreen()).then((value){
+            expect(find.byType(LoadingElevatedButton),findsOneWidget);
+          });
+      });
+  });
+}
+
